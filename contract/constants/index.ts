d3613f0fc4cb085daf3922adaaf37908dc79e493
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-10-24 14:27:43 +0100

    contract

---
+++ contract/constants/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x71080c6963C5e7aedfBC1137e325a7B35A08249c";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL =
+  "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
